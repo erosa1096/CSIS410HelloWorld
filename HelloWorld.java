aabf40f8a429da31e86e5a9a0408d376917b71c0
erosa1096 <31660968+erosa1096@users.noreply.github.com>
2017-10-05 17:54:48 -0400

    Hello World with Spanish greeting

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class HelloWorld here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class HelloWorld
+{
+    public static void main(String[] agrs){
+        System.out.println("Hello World, Como Estas?");
+    }
+}
